af728c8392414321770e76738a7265f1790d8a03
Patel Rahul <rahulpatel16092005@gmail.com>
2026-03-30 17:26:37 +0530

    solve problem

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-def add(a,b):return a+b
+def add(a, b):
+    return a + b
 
 
+def sub(a, b):
+    return a - b
 
 
+result1=add(3, 4)
+result2=sub(3, 2)
 
-
-def sub(a,b):
-    
-    return a-b
-
-
-result1=add(3,4)
-result2=sub(3,2)
-
-print(result1,result2)
+print(result1, result2)
